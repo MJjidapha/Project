35bc36ffa3a059e78edd5ef7572904253684fcda
MJjidapha <moji_keroro@hotmail.com>
2018-06-23 14:30:43 +0700

    Add files via upload

--- python/post.py
+++ python/post.py
@@ -1,4 +1,32 @@
+import sqlite3
+from firebase import firebase
+firebase = firebase.FirebaseApplication('https://dogwood-terra-184417.firebaseio.com/', None)
+def search_object_Train():
+    with sqlite3.connect("db1.db") as con:
+        cur = con.cursor()
+        #cur.execute('SELECT * FROM object_Train WHERE name=?',(name,))
+        try :
+            cur.execute("SELECT *FROM action")
+            rows = cur.fetchall()
+           
+            for element in rows:
+                list = element
+                print list
+                firebase.post('/action/data', {'action_name': str(list[0]),'motor':str(list[2:10]),'step':list[1]})
+        except :
+            return "None"
+       # return cur.fetchone() # None OR (u'Ball', 2)
+       
+search_object_Train()
+
+#list = search_object_Train("grab")
+#print list[2:10]
+
+
 
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://dogwood-terra-184417.firebaseio.com/', None)
-add = firebase.post('/action/data', {'action_name': 'Tong','motor':'(1,2,3,4,5,6,7,8)','step':'1'})
+result = firebase.get('/Gauges/data/',None)
+if result!="None" :
+    result = firebase.delete('/Gauges/data/',None)     
+firebase.post('/Gauges/data', {'Battery':float(65.22),'temperature':float(20.55)})
